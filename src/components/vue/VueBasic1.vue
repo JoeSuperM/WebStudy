884de70c9fe2205ae330e1d0b1bab34ca985308c
zhouchao <1073607866@qq.com>
2020-04-24 17:40:50 +0800

    提交少了的文件

---
+++ src/components/vue/VueBasic1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="center"><b>{{title}}</b></h1>
+    <hr />
+    <div class="content">
+      <h1>一、Vue简介</h1>
+    </div>
+    <br />
+  </div>
+</template>
+<style scoped>
+@import url('../../style/right_content.css');
+
+</style>
+<script>
+export default {
+  name: 'VueBasic1',
+  data() {
+    return {
+      title: 'Vue基础--简介、基础语法'
+    }
+  }
+}
+
+</script>
